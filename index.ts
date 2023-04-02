bb57fd2383c7413589667d6873e41489bae01970
Chris Sams <chris_sams92@hotmail.com>
2023-04-02 11:21:55 -0700

    add support for common http errors

--- index.ts
+++ index.ts
@@ -1,8 +1,67 @@
 import { Request, Response, NextFunction } from 'express';
 
-function handleErrors(err: Error, req: Request, res: Response, next: NextFunction) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+class HttpError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
 
-export default handleErrors;
+class UnauthorizedError extends HttpError {
+  constructor(message?: string) {
+    super(message || 'Unauthorized', 401);
+  }
+}
+
+class ForbiddenError extends HttpError {
+  constructor(message?: string) {
+    super(message || 'Forbidden', 403);
+  }
+}
+
+class NotFoundError extends HttpError {
+  constructor(message?: string) {
+    super(message || 'Not Found', 404);
+  }
+}
+
+class StateConflict extends HttpError {
+  constructor(message?: string) {
+    super(message || 'State Conflict', 409);
+  }
+}
+
+class UnprocessableEntityError extends HttpError {
+  constructor(message?: string) {
+    super(message || 'Unprocessable Entity', 422);
+  }
+}
+
+class TooManyRequestsError extends HttpError {
+  constructor(message?: string) {
+    super(message || 'Too Many Requests', 429);
+  }
+}
+
+function handleErrors(err: Error, req: Request, res: Response, next: NextFunction) {
+  console.error(err.stack);
+
+  if (err instanceof HttpError) {
+    res.status(err.statusCode).send(err.message);
+  } else {
+    res.status(500).send('Something broke!');
+  }
+}
+
+export {
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  StateConflict,
+  UnprocessableEntityError,
+  TooManyRequestsError,
+  handleErrors,
+};
